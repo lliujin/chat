df7f8c80df4027b29408a3dba8dd7252d4175ef8
liujin <389594458@qq.com>
2019-03-19 15:35:15 +0800

    Chat0.9

--- src/com/liujin/ChatServer.java
+++ src/com/liujin/ChatServer.java
@@ -8,24 +8,33 @@ import java.net.Socket;
 public class ChatServer {
     public static void main(String[] args) {
 
+        Socket s = null;
+        DataInputStream dis = null;
+
         boolean started = false;
         try {
             ServerSocket ss = new ServerSocket(9999);
             started = true;
             while(started) {
                 boolean bConnected = false;
-                Socket s = ss.accept();
+                s = ss.accept();
 System.out.println("connected!");
                 bConnected = true;
-                DataInputStream dis = new DataInputStream(s.getInputStream());
+                dis = new DataInputStream(s.getInputStream());
                 while(bConnected) {
                     String str = dis.readUTF();
                     System.out.println(str);
                 }
-                dis.close();
             }
         } catch (IOException e) {
-            e.printStackTrace();
+            System.out.println("Client closed!");
+        } finally {
+            try {
+                if(dis != null) dis.close();
+                if(s != null) s.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
