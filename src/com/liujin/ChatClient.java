ff2490f42d1f0f35da0bbc8b34d8863e745a951d
liujin <389594458@qq.com>
2019-03-19 10:00:32 +0800

    Chat0.2

--- src/com/liujin/ChatClient.java
+++ src/com/liujin/ChatClient.java
@@ -3,12 +3,19 @@ package com.liujin;
 import java.awt.*;
 
 public class ChatClient extends Frame {
+
+    TextField textField = new TextField();
+    TextArea textArea = new TextArea();
+
     public static void main(String[] args) {
         new ChatClient().launchFrame();
     }
 
     public void launchFrame() {
         setBounds(400,300,300,300);
+        add(textField, BorderLayout.SOUTH);
+        add(textArea, BorderLayout.NORTH);
+        pack();
         setVisible(true);
     }
 }
