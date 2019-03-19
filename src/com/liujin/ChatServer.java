ef1ff29a98cf6443311948f544035465174d50f8
liujin <389594458@qq.com>
2019-03-19 17:45:44 +0800

    Chat1.0

--- src/com/liujin/ChatServer.java
+++ src/com/liujin/ChatServer.java
@@ -8,15 +8,18 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class ChatServer {
+
+    boolean started = false;
+    ServerSocket ss = null;
+
     public static void main(String[] args) {
+        new ChatServer().start();
+    }
 
-        ServerSocket ss = null;
-        Socket s = null;
-        DataInputStream dis = null;
-        boolean started = false;
-
+    public void start() {
         try {
             ss = new ServerSocket(9999);
+            started = true;
         } catch (BindException e) {
             System.out.println("端口使用中……");
             System.out.println("请关掉相关程序并重新启动服务器");
@@ -25,28 +28,60 @@ public class ChatServer {
             e.printStackTrace();
         }
         try {
-            started = true;
+
             while(started) {
-                boolean bConnected = false;
-                s = ss.accept();
-System.out.println("connected!");
-                bConnected = true;
-                dis = new DataInputStream(s.getInputStream());
-                while(bConnected) {
-                    String str = dis.readUTF();
-                    System.out.println(str);
-                }
+                Socket s = ss.accept();
+                Client c = new Client(s);
+
+                new Thread(c).start();
+                System.out.println("connected!");
+
             }
-        } catch(EOFException e) {
-            System.out.println("Client closed!");
-        } catch (IOException e) {
+        } catch(IOException e) {
             e.printStackTrace();
         } finally {
             try {
-                if(dis != null) dis.close();
-                if(s != null) s.close();
+                ss.close();
             } catch (IOException e) {
                 e.printStackTrace();
+            }
+        }
+    }
+
+    class Client implements Runnable {
+
+        private Socket s;
+        private DataInputStream dis = null;
+        private boolean bConnected = false;
+
+        public Client(Socket s) {
+            this.s = s;
+            try {
+                dis = new DataInputStream(s.getInputStream());
+                bConnected = true;
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        @Override
+        public void run() {
+            try {
+                while (bConnected) {
+                    String str = dis.readUTF();
+                    System.out.println(str);
+                }
+            } catch(EOFException e){
+                    System.out.println("Client closed!");
+            } catch(IOException e){
+                    e.printStackTrace();
+            } finally{
+                try {
+                    if (dis != null) dis.close();
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
             }
         }
     }
