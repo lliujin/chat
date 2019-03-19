68363c5aaf8f49b7a6ea4921a04164c5c0bda443
liujin <389594458@qq.com>
2019-03-19 10:44:03 +0800

    Chat0.4

--- src/com/liujin/ChatClient.java
+++ src/com/liujin/ChatClient.java
@@ -1,6 +1,8 @@
 package com.liujin;
 
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
@@ -25,6 +27,17 @@ public class ChatClient extends Frame {
                 System.exit(0);
             }
         });
+        textField.addActionListener(new TextFieldListener());
         setVisible(true);
+    }
+
+    private class TextFieldListener implements ActionListener {
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            String s = textField.getText();
+            textArea.setText(s);
+            textField.setText("");
+        }
     }
 }
