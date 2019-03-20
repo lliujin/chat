d37b4a140f8b6c3a12b35f2c2b0377afeba47754
liujin <389594458@qq.com>
2019-03-20 11:15:00 +0800

    Chat1.3.2

--- src/com/liujin/ChatServer.java
+++ src/com/liujin/ChatServer.java
@@ -7,7 +7,6 @@ import java.io.IOException;
 import java.net.BindException;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.net.SocketException;
 import java.util.ArrayList;
 import java.util.List;
 
